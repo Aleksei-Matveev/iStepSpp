8c0d6ccad2d4b6f56b265f315d8ae9388f7ef049
stoma-siarhei <scander@yandex.ru>
2016-04-23 07:21:19 +0300

    IO_Stream

--- IO_Stream/IO_Stream.cpp
+++ IO_Stream/IO_Stream.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <iomanip>
 #include <cstdlib>
 
 using namespace std;
@@ -13,6 +14,55 @@ int main()
 		cout.width(10);	// Выравниваем длину
 		cout << values[i] << '\n';
 	}
+	system("pause");
+	for (int i = 0; i < 4; i++)	{
+		cout.width(10);
+		cout.fill('*'); // Заполнители
+		cout << values[i] << endl; // Переход на ноовую строку
+	}
+	cout.fill('_');
+	system("pause");
+	char *names[] = { "Zoot", "Jimmy", "Al", "Stan" };
+	for (int i = 0; i < 4; i++) 
+		cout << setw(6) << names[i] // Длина names 6 символов
+			<< setw(10) << values[i] << endl; // Длина values 10 символов
+	cout.fill(' ');
+	system("pause");
+	for (int i = 0; i < 4; i++)
+		cout << setiosflags(ios::left) // Выравнивание по левому краю
+		<< setw(6) << names[i]
+		<< resetiosflags(ios::left) // Отмена выравнивания по левому краю
+		<< setw(10) << values[i] << endl;
+	system("pause");
+	cout << setiosflags(ios::fixed); // Фиксируем точный вывод
+	for (int i = 0; i < 4; i++)
+		cout << setiosflags(ios::left)
+		<< setw(6)
+		<< names[i]
+		<< resetiosflags(ios::left)
+		<< setw(10)
+		<< setprecision(1) // Количество цифр после запятой
+		<< values[i]
+		<< endl;
+	system("pause");
+	int mass[] = { 123, 3536, -6537, 435824 };
+	for (int i = 0; i < 4; i++)
+		cout << setiosflags(ios::left)
+		<< setw(6)
+		<< names[i]
+		<< resetiosflags(ios::left)
+		<< setw(10)
+		<< hex
+		<< mass[i]
+		<< setfill('.')
+		<< setw(10)
+		<< dec
+		<< mass[i]
+		<< setfill(' ')
+		<< setw(14)
+		<< oct
+		<< mass[i]
+		<< endl;
 	system("pause");
 	return 0;
 }
